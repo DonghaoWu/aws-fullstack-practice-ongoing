c355726b3545021daa0e102659764c552f5459de
Donghao Wu <donghao.wu.us@gmail.com>
2021-05-11 16:47:39 -0400

    add part 2 readme and code.

--- aws-node-practice/src/config/config.ts
+++ aws-node-practice/src/config/config.ts
@@ -1,19 +1,22 @@
 export const config = {
-  "dev": {
-      "username": process.env.POSTGRE_USERNAME,
-      "password": process.env.POSTGRE_PASSWORD,
-      "database": process.env.POSTGRE_DATABASE,
-      "host": process.env.POSTGRE_HOST,
-      "dialect": process.env.DIALECT,
-      "aws_region": process.env.AWS_REGION,
-      "aws_profile": process.env.AWS_PROFILE,
-      "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
-  },
-  "prod": {
-      "username": "",
-      "password": "",
-      "database": "",
-      "host": "",
-      "dialect": "postgres"
-  }
+    "dev": {
+        "username": process.env.POSTGRE_USERNAME,
+        "password": process.env.POSTGRE_PASSWORD,
+        "database": process.env.POSTGRE_DATABASE,
+        "host": process.env.POSTGRE_HOST,
+        "dialect": process.env.DIALECT,
+        "aws_region": process.env.AWS_REGION,
+        "aws_profile": process.env.AWS_PROFILE,
+        "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
+    },
+    "prod": {
+        "username": "",
+        "password": "",
+        "database": "",
+        "host": "",
+        "dialect": "postgres"
+    },
+    "jwt": {
+        "secret": process.env.JWT_SECRET
+    }
 }
